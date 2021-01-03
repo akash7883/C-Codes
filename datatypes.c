f3beb9d1685e0b6f3401e935c3748c21def99bdb
Akash porwal <dharya2001porwal@gmail.com>
2021-01-03 21:45:49 +0530

    Add files via upload

---
+++ datatypes.c
@@ -0,0 +1,22 @@
+// C Codes For basic datatypes
+#include<stdio.h>
+int main()
+{
+	//int datatype
+	int a;
+	a=10;
+	printf("Integer Datatype=%d",a);
+	float b;
+	b=10.5000000;
+	printf("\nFloating Datatype=%f",b);
+	double c;
+	c=11.56789123;
+	printf("\nDouble Datatype=%lf",c);
+	char d;
+	d='A';
+	printf("\nChar =%c",d);
+	
+	
+return 0;
+}
+
